e748ac105537a12428d190aec49702d0ecc2f4cb
abbasanwar158 <abbas.anwar@devbox.co>
2022-01-31 15:59:20 +0500

    login set

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,13 +1,24 @@
 import React, { useState, useContext, useEffect } from "react";
 import TextField from '@material-ui/core/TextField';
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiFilledInput-root": {
+      background: "rgb(232, 241, 250)"
+    }
+  }
+}));
 
 export default function Login() {
+  const classes = useStyles();
   return (
     <div className="loginContainer" id="test">
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-6 text-center mb-5">
-            <h2 className="heading-section">Login123</h2>
+            <h2 className="heading-section text-white">Login</h2>
           </div>
         </div>
         <div className="row justify-content-center">
@@ -18,6 +29,8 @@ export default function Login() {
             id="login_outlined-basic-email"
             label="Email"
             type="text"
+            variant="filled"
+            className={classes.root}
             fullWidth
           />
         </div>
@@ -26,6 +39,8 @@ export default function Login() {
             id="login_outlined-basic-email"
             label="Password"
             type="password"
+            variant="filled"
+            className={classes.root}
             fullWidth
           />
         <span toggle="#password-field" className="fa fa-fw fa-eye field-icon toggle-password"></span>
@@ -33,8 +48,11 @@ export default function Login() {
         <div className="form-group">
           <button type="submit" className="form-control btn btn-primary submit px-3">Sign In</button>
         </div>
-        <div>
-          <span>Forget Password?</span>
+        <div className="mt-3">
+          <a className="text-white cursor-pointer">Forget Password?</a>
+          <div className="d-flex">
+            <a className="text-white mt-1 cursor-pointer mx-auto">Sign Up</a>
+          </div>
         </div>
         </div>
         </div>
